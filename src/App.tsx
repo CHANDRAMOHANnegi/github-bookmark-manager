26d43fe26d731bcf107bb986fe173ec4577cc274
CHANDRAMOHANnegi <cmsingh1721997@gmail.com>
2021-01-17 21:12:10 +0530

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
+import React, { useEffect ,Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    fetch('https://api.github.com/organizations').then(d => d.json()).then((d) => {
+      console.log(d)
+    }).catch(err => {
+      console.log(err);
+    })
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
